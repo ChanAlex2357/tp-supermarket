74dbc7c6eb35ee597c7223fd300d964990967cd4
TahinaBemax <153293127+TahinaBemax@users.noreply.github.com>
2024-06-17 15:38:36 +0300

    pages terminées

--- application/views/home.php
+++ application/views/home.php
@@ -10,15 +10,16 @@
                 <a href="http://localhost/tp-supermarket/Caisse" class="list-group-item">Menu 3</a>
             </div>
         </div>
+
         <!-- /.col-lg-3 -->
-        <div class="col-lg-9">
+<!--        <div class="col-lg-9">
             <div class="row">
                 <div class="col-md-12">
                     <h1> content </h1>
                 </div>
             </div>
-            <!-- /.row -->
-        </div>
+             /.row
+        </div>-->
         <!-- /.col-lg-9 -->
 
     </div>
