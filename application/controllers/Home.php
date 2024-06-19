8ed7d1ecb805ea42d5b68464eaac99ee46aa1c53
TahinaBemax <153293127+TahinaBemax@users.noreply.github.com>
2024-06-19 17:49:30 +0300

    login terminé sady nety ny session

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -3,9 +3,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Home extends CI_Controller {
     public function index()
     {
-        $this->load->view('inc/header.php');
+        $this->load->view('inc/header.php', ['title' => "Home"]);
         $this->load->view('inc/nav-bar.php');
-        $this->load->view('home.php', ['title' => "Home"]);
+        $this->load->view('home.php');
         $this->load->view('inc/footer.php');
     }
 }
